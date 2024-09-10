9db8bd4ee3c758628f2ac4eeacb7e4c22e03b1e7
Marslino Ashraf <marslinoashraf8@gmail.com>
2024-09-10 19:06:32 +0300

    Add project files.

---
+++ Airport/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Airport
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label2 = new Label();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.InactiveCaptionText;
+            label1.Font = new Font("Chiller", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.IndianRed;
+            label1.Image = Properties.Resources.baby_blue_background_2gskjqwz1lzomptm;
+            label1.Location = new Point(274, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 43);
+            label1.TabIndex = 0;
+            label1.Text = "Developers";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Cyan;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Comic Sans MS", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Image = Properties.Resources.images__1_3;
+            button1.Location = new Point(258, 357);
+            button1.Name = "button1";
+            button1.Size = new Size(196, 45);
+            button1.TabIndex = 5;
+            button1.Text = "Let's Start";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.HighlightText;
+            label3.Font = new Font("Monotype Corsiva", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(260, 231);
+            label3.Name = "label3";
+            label3.Size = new Size(153, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Patrick Nassef";
+            label3.Click += label3_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.HighlightText;
+            label5.Font = new Font("Monotype Corsiva", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label5.Location = new Point(258, 287);
+            label5.Name = "label5";
+            label5.Size = new Size(175, 28);
+            label5.TabIndex = 4;
+            label5.Text = "Marslino Ashraf ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.HighlightText;
+            label4.Font = new Font("Monotype Corsiva", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.Location = new Point(264, 203);
+            label4.Name = "label4";
+            label4.Size = new Size(149, 28);
+            label4.TabIndex = 3;
+            label4.Text = "Amera Ashraf ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.HighlightText;
+            label2.Font = new Font("Monotype Corsiva", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(264, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(160, 28);
+            label2.TabIndex = 6;
+            label2.Text = "Arsany Morcos ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.HighlightText;
+            label6.FlatStyle = FlatStyle.Flat;
+            label6.Font = new Font("Monotype Corsiva", 18F, FontStyle.Bold | FontStyle.Italic);
+            label6.Location = new Point(264, 259);
+            label6.Name = "label6";
+            label6.Size = new Size(108, 28);
+            label6.TabIndex = 7;
+            label6.Text = "Sama Amr";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.web_developer_illustration_1004x1024_wcqgbag3;
+            pictureBox1.Location = new Point(12, 156);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(166, 159);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.Egyptair_Logo_20102;
+            pictureBox4.Location = new Point(122, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(457, 86);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 11;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources._7069889;
+            pictureBox3.Location = new Point(529, 156);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(172, 159);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 12;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.mam_logo2;
+            pictureBox5.Location = new Point(378, 445);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(250, 78);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 14;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.images5;
+            pictureBox6.Location = new Point(194, 421);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(136, 116);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 15;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.images6;
+            pictureBox8.Location = new Point(336, 422);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(36, 115);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 17;
+            pictureBox8.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(728, 551);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox1);
+            Controls.Add(label6);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Label label3;
+        private Label label5;
+        private Label label4;
+        private Label label2;
+        private Label label6;
+        private PictureBox pictureBox1;
+        private Label label7;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox8;
+    }
+}
